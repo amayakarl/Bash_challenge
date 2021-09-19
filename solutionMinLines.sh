899fba186e7ac0ec950704001afb2b791f02c2a0
Karl Amaya <aakarl32@gmail.com>
2021-09-19 01:04:44 -0600

    Update solutionMinLines.sh

--- solutionMinLines.sh
+++ solutionMinLines.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+exitCode=0
 # create the starter folder and change working directory to the starter folder
 mkdir starter
 cd starter
 # cheat using awk :P
 # replace create-* and delete-* with their bash counterparts then pipe the output to bash to run it
-$(awk '{gsub(/create-directory/, "mkdir");gsub(/create-file/, "touch");gsub(/delete-directory/, "rm -Rf");gsub(/delete-file/, "rm");}1' ../hier.txt | bash) 
+($(awk '{gsub(/create-directory/, "mkdir");gsub(/create-file/, "touch");gsub(/delete-directory/, "rm -Rf");gsub(/delete-file/, "rm");}1' ../hier.txt | bash)) || ((exitCode+=1))
+exit $exitCode
